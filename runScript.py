863ba91149600bbeba86ebee65a76fc01b4401b8
AS558 <6310110558@psu.ac.th>
2023-09-30 22:36:59 +0700

    save log to MySQL

--- runScript.py
+++ runScript.py
@@ -1,53 +1,75 @@
 import os
-import io
 import subprocess
 import time
+import datetime
+import csv
 import mysql.connector
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
 
 mysql_config = {
-    'user': 'your_username',
-    'password': 'your_password',
+    'user': 'root',
     'host': 'localhost',
-    'database': 'your_database',
+    'database': 'warehouse_db',
 }
-
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
-SERVICE_ACCOUNT_FILE = 'your-service-account-json-file.json'
 
 conn = mysql.connector.connect(**mysql_config)
 cursor = conn.cursor()
-
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-drive_service = build('drive', 'v3', credentials=credentials)
 
 weights_path = "./runs/train/exp4/weights/best.pt"
 image_size = 640
 confidence_threshold = 0.25
 source_folder = './buffer'
-temp_folder = './runs/detect'
+temp_folder = './runs/detect/exp'
 
-def delete_file(src):
+def delete_file(source):
     try:
-        for root, dirs, files in os.walk(src, topdown=False):
+        for root, dirs, files in os.walk(source, topdown=False):
             for file in files:
                 file_path = os.path.join(root, file)
                 os.remove(file_path)
             for dir in dirs:
                 dir_path = os.path.join(root, dir)
                 os.rmdir(dir_path)
-        print(f"Delect file in {src} complete.")
+        print(f"Delect file in {source} complete.")
     except Exception as e:
         print(f"Error deleting file {file_path}: {str(e)}")
+
+def process_prediction_csv():
+    try:
+        with open(f'{temp_folder}/predictions.csv', newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                if len(row) >= 3:
+                    filename = row[0]
+                    classification = row[1]
+                    confidence = float(row[2])
+                    timestamp = datetime.datetime.now()
+                    original_image_path = os.path.join(source_folder, filename)
+                    result_image_path = os.path.join(temp_folder, filename)
+
+                    insert_data_into_mysql(filename, classification, confidence, timestamp, original_image_path, result_image_path)
+            
+            delete_file(f"../{temp_folder}")
+
+    except Exception as e:
+        print(f"Error processing predictions.csv: {str(e)}")
+
+def insert_data_into_mysql(filename, classification, confidence, timestamp, original_image_path, result_image_path):
+    try:
+        sql = "INSERT INTO cement_stock_list (file_name, classification, confidence, timestamp, original_image_path, result_image_path) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (filename, classification, confidence, timestamp, original_image_path, result_image_path)
+        cursor.execute(sql, values)
+        conn.commit()
+        print(f"Data inserted into MySQL: {filename}, {classification}, {confidence}, {timestamp}")
+    except Exception as e:
+        print(f"Error inserting data into MySQL: {str(e)}")
 
 def test_yolov5():
     print(f"Detected new file in the Buffer folder. detect.py is Running.")
     command = f'python detect.py --weight {weights_path} --img {image_size} --conf {confidence_threshold} --source {source_folder} --save-csv'
     subprocess.run(command, shell=True)
+    process_prediction_csv()
     delete_file(source_folder)
 
 class BufferHandler(FileSystemEventHandler):
@@ -69,6 +91,5 @@ try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    delete_file(temp_folder)
     observer.stop()
     observer.join()
